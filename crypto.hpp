0fe021189c142c89415228a879a84fe46b01eced
ninameed <meedby@gmail.com>
2017-09-12 15:53:35 +0200

    Update crypto.hpp

--- crypto.hpp
+++ crypto.hpp
@@ -3,6 +3,8 @@
 #include <sstream>
 #include <iomanip>
 #include <string>
+#include <openssl/md5.h>
+#include <stdio.h>
 
 // Note: C-style casts, for instance (int), are used to simplify the source code.
 //       C++ casts, such as static_cast and reinterpret_cast, should otherwise
@@ -22,7 +24,11 @@ public:
 
   /// Return the MD5 (128-bit) hash from input.
   static std::string md5(const std::string &input) {
-    throw std::logic_error("not yet implemented");
+    //throw std::logic_error("not yet implemented");
+    std::string hash;
+    hash.resize(128/8);
+    MD5((const unsigned char *)input.data(), input.size(), (unsigned char *)hash.data());
+    return hash;
   }
 
   /// Return the SHA-1 (160-bit) hash from input.
@@ -35,12 +41,20 @@ public:
 
   /// Return the SHA-256 (256-bit) hash from input.
   static std::string sha256(const std::string &input) {
-    throw std::logic_error("not yet implemented");
+    //throw std::logic_error("not yet implemented");
+    std::string hash;
+    hash.resize(256/8);
+    SHA256((const unsigned char *)input.data(), input.size(), (unsigned char *)hash.data());
+    return hash;
   }
 
   /// Return the SHA-512 (512-bit) hash from input.
   static std::string sha512(const std::string &input) {
-    throw std::logic_error("not yet implemented");
+    //throw std::logic_error("not yet implemented");
+    std::string hash;
+    hash.resize(512/8);
+    SHA512((const unsigned char *)input.data(), input.size(), (unsigned char *)hash.data());
+    return hash;
   }
 
   /// Return key from the Password-Based Key Derivation Function 2 (PBKDF2).
