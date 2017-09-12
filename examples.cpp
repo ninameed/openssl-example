d8b33978cbf6a3c11aa65c8d5da544cd0d467f0b
ninameed <meedby@gmail.com>
2017-09-12 15:57:28 +0200

    Update examples.cpp

--- examples.cpp
+++ examples.cpp
@@ -9,6 +9,15 @@ int main() {
   
   cout << "SHA-1 with two iterations" << endl;
   cout << Crypto::hex(Crypto::sha1(Crypto::sha1("Test"))) << endl;
+  
+  cout << "MD5 with 1 iteration" << endl;
+  cout << Crypto::hex(Crypto::md5("Test")) << endl;
+  
+  cout << "SHA-256 with 1 iteration" << endl;
+  cout << Crypto::hex(Crypto::sha256("Test")) << endl;
+  
+  cout <<"SHA-512 with 1 iteration" << endl;
+  cout << Crypto::hex(Crypto::sha512("Test")) << endl;
 
   cout << "The derived key from the PBKDF2 algorithm" << endl;
   cout << Crypto::hex(Crypto::pbkdf2("Password", "Salt")) << endl;
